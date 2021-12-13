770d88cf25023eae241f68eff0448ef284f65e76
MrMohammadY <m.yazdani000@gmail.com>
2021-12-13 18:28:08 +0330

    Create(accounts/admin): create user admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
 
-# Register your models here.
+User = get_user_model()
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'username', 'email', 'first_name', 'last_name', 'is_staff', 'is_active', 'date_joined')
+    list_filter = ('is_staff', 'is_active')
+    search_fields = ('username', 'email')
+    list_display_links = ('id', 'username', 'email')
