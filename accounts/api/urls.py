bcdf9b435bbd4edbbb689b8898b943c6d5269573
MrMohammadY <m.yazdani000@gmail.com>
2021-12-14 17:22:42 +0330

    Create(accounts/api/urls): create user profile url

--- accounts/api/urls.py
+++ accounts/api/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from rest_framework_simplejwt import views as jwt_views
 
-from accounts.api.views import RegistrationCreateAPIView
+from accounts.api.views import RegistrationCreateAPIView, UserProfileRetrieveAPIView
 
 app_name = 'api'
 
@@ -9,4 +9,5 @@ urlpatterns = [
     path('token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
     path('registration/', RegistrationCreateAPIView.as_view(), name='registration'),
+    path('profile/', UserProfileRetrieveAPIView.as_view(), name='profile'),
 ]
