dff89081e60a2e038724b9b8fe9e203ce1c36f79
MrMohammadY <m.yazdani000@gmail.com>
2021-12-14 10:53:13 +0330

    Create: create home page

--- bmi/urls.py
+++ bmi/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
+    path('home/', TemplateView.as_view(template_name='home.html'), name='home'),
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls', namespace='accounts')),
 ]
