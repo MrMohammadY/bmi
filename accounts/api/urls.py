2b0869fe64ebc38a41a40f8844beee299e94a898
MrMohammadY <m.yazdani000@gmail.com>
2021-12-14 17:09:07 +0330

    Create(JWT): install and config jwt

--- accounts/api/urls.py
+++ accounts/api/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
+from rest_framework_simplejwt import views as jwt_views
 
 from accounts.api.views import RegistrationCreateAPIView
 
 app_name = 'api'
 
 urlpatterns = [
+    path('token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
     path('registration/', RegistrationCreateAPIView.as_view(), name='registration'),
 ]
--- bmi/settings.py
+++ bmi/settings.py
@@ -34,7 +34,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
-    
+
     'accounts.apps.AccountsConfig',
     'bmi_calculator.apps.BmiCalculatorConfig',
 ]
@@ -128,3 +128,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'accounts.User'
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.SessionAuthentication'
+    ],
+}
