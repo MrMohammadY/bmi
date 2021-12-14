6041e49844117cad7461056786bf58935249d8bb
MrMohammadY <m.yazdani000@gmail.com>
2021-12-14 17:22:32 +0330

    Create(accounts/api/views): create user profile api views

--- accounts/api/views.py
+++ accounts/api/views.py
@@ -1,7 +1,8 @@
 from django.contrib.auth import get_user_model
-from rest_framework.generics import CreateAPIView
+from rest_framework.generics import CreateAPIView, RetrieveAPIView
+from rest_framework.permissions import IsAuthenticated
 
-from accounts.api.serializers import RegistrationSerializer
+from accounts.api.serializers import RegistrationSerializer, ProfileSerializer
 
 User = get_user_model()
 
@@ -9,3 +10,11 @@ User = get_user_model()
 class RegistrationCreateAPIView(CreateAPIView):
     queryset = User.objects.all()
     serializer_class = RegistrationSerializer
+
+
+class UserProfileRetrieveAPIView(RetrieveAPIView):
+    permission_classes = (IsAuthenticated,)
+    serializer_class = ProfileSerializer
+
+    def get_object(self):
+        return self.request.user
